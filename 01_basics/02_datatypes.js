528abc843eabe43bb2fe5738f7c6a5de52a3aae8
HRCoders <146632126+HRCoders@users.noreply.github.com>
2024-04-12 16:05:27 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+console.log("Manish");
+let name="Manish"             
+let age= 18
+let isLoggedIn= false
+let state = null
+
+  /* Number => 2 to power 53
+     bigint
+     string => ""
+     boolean => true/false\
+     null => standalone value
+     undefined => value not assigned yet
+     symbol => when we talk about unique
+  */
+ 
+ console.log(typeof null); // object
+ console.log(typeof undefined); // undefined
+
+
+
