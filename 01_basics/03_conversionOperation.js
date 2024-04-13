0eb148c7d7c6f98e33f67aa8c3eb55d01a04cbcf
HRCoders <146632126+HRCoders@users.noreply.github.com>
2024-04-13 10:18:31 +0000

    conversion done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,13 +7,13 @@
 
 let score= "33abc"
 
-console.log(typeof score);
-   // OR we write also as below , both are same
-console.log(typeof(score));
+// console.log(typeof score);
+//    // OR we write also as below , both are same
+// console.log(typeof(score));
 
 let valueInNumber= Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 /* conversion into boolean  result
@@ -24,8 +24,8 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn= Boolean( isLoggedIn );
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
   
   
 /* conversion into string result
@@ -34,5 +34,67 @@ console.log(booleanIsLoggedIn);
 */
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//****************Operations***************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);  //  for power 2 raise to power 3
+// console.log(2/2);
+// console.log(2%3);   // for reminder
+
+let str1 = "Hello"
+let str2 = " Manish"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1"+ 2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+//console.log(3+4*5%3); // wrong way to writecode
+//console.log((3+4)*5%3); // best way to write code , use paranthisis
+
+
+/*      ****Prefix and Postfix****
+ link to study
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+*/
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+let runCounter = 100
+runCounter++
+console.log(runCounter);
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+
+
+
+
