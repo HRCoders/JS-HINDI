e40125c0743e9d10e13cf2ddfd03a3ab5ff2a4dc
HRCoders <146632126+HRCoders@users.noreply.github.com>
2024-04-16 18:06:06 +0000

    string done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,70 @@
+const name = "manish"
+const repoCount = 30
+
+// console.log(name + repoCount + " value"); // NOT ACCEPTABLE
+// console.log(`Hello mmy name is ${name} and my repocount is ${repoCount}`); // ACCEPTABLE
+
+
+/****************** String Functions or Methods (prototype) *********************/
+// length: .length
+// big: ƒ big()
+// blink: ƒ blink()
+// bold: ƒ bold()
+// charAt: ƒ charAt()
+// charCodeAt: ƒ charCodeAt()
+// codePointAt: ƒ codePointAt()
+// concat: ƒ concat()
+// constructor: ƒ String()
+// endsWith: ƒ endsWith()
+// fixed: ƒ fixed()
+// fontcolor: ƒ fontcolor()
+// fontsize: ƒ fontsize()
+// includes: ƒ includes()
+// indexOf: ƒ indexOf()
+// isWellFormed: ƒ isWellFormed()
+// italics: ƒ italics()
+// lastIndexOf: ƒ lastIndexOf()
+// length: 0
+// link: ƒ link()
+// localeCompare: ƒ localeCompare()
+// match: ƒ match()
+// matchAll: ƒ matchAll()
+// normalize: ƒ normalize()
+// padEnd: ƒ padEnd()
+// padStart: ƒ padStart()
+// repeat: ƒ repeat()
+// replace: ƒ replace()
+// replaceAll: ƒ replaceAll()
+// search: ƒ search()
+// slice: ƒ slice()
+// small: ƒ small()
+// split: ƒ split()
+// startsWith: ƒ startsWith()
+// strike: ƒ strike()
+// sub: ƒ sub()
+// substr: ƒ substr()
+// substring: ƒ substring()
+// sup: ƒ sup()
+// toLocaleLowerCase: ƒ toLocaleLowerCase()
+// toLocaleUpperCase: ƒ toLocaleUpperCase()
+// toLowerCase: ƒ toLowerCase()
+// toString: ƒ toString()
+// toUpperCase: ƒ toUpperCase()
+// toWellFormed: ƒ toWellFormed()
+// trim: ƒ trim()
+// trimEnd: ƒ trimEnd()
+// trimLeft: ƒ trimStart()
+// trimRight: ƒ trimEnd()
+// trimStart: ƒ trimStart()
+// valueOf: ƒ valueOf()
+// Symbol(Symbol.iterator): ƒ [Symbol.iter
+
+const gameName = new String("ladiisharma")
+
+// console.log(gameName[1]);
+// console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('d'));
