db712622272b33a48207707cc8b79cafef2b7a89
HRCoders <146632126+HRCoders@users.noreply.github.com>
2024-04-19 17:29:53 +0000

    number done

--- 01_basics/06_number_math.js
+++ 01_basics/06_number_math.js
@@ -1,13 +1,86 @@
 const score = 400
 const balance = new Number(200)
 //console.log(balance);
-console.log(balance.toString());
-console.log(balance.toFixed(1));
+// console.log(balance.toString()); // to convert in to string
+// console.log(balance.toFixed(3)); // to add specified number of zeros after dot (200.000)
 
 
 const otherNumber = 123.8966
-console.log(otherNumber.toPrecision (3));
+//console.log(otherNumber.toPrecision (3)); // to precise number
 
 
-const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'));
+const hundreds = 1000000    
+//console.log(hundreds.toLocaleString('en-IN')); // to change the system of comma inserting between number (10,00,000)
+
+
+/***********************MATH********************* */
+ //------------ MATH FUNCTIONS/METHODS-----------
+
+// E: 2.718281828459045
+// LN2: 0.6931471805599453
+// LN10: 2.302585092994046
+// LOG2E: 1.4426950408889634
+// LOG10E: 0.4342944819032518
+// PI: 3.141592653589793
+// SQRT1_2: 0.7071067811865476
+// SQRT2: 1.4142135623730951
+// abs: ƒ abs()
+// acos: ƒ acos()
+// acosh: ƒ acosh()
+// asin: ƒ asin()
+// asinh: ƒ asinh()
+// atan: ƒ atan()
+// atan2: ƒ atan2()
+// atanh: ƒ atanh()
+// cbrt: ƒ cbrt()
+// ceil: ƒ ceil()
+// clz32: ƒ clz32()
+// cos: ƒ cos()
+// cosh: ƒ cosh()
+// exp: ƒ exp()
+// expm1: ƒ expm1()
+// floor: ƒ floor()
+// fround: ƒ fround()
+// hypot: ƒ hypot()
+// imul: ƒ imul()
+// log: ƒ log()
+// log1p: ƒ log1p()
+// log2: ƒ log2()
+// log10: ƒ log10()
+// max: ƒ max()
+// min: ƒ min()
+// pow: ƒ pow()
+// random: ƒ random()
+// round: ƒ round()
+// sign: ƒ sign()
+// sin: ƒ sin()
+// sinh: ƒ sinh()
+// sqrt: ƒ sqrt()
+// tan: ƒ tan()
+// tanh: ƒ tanh()
+// trunc: ƒ trunc()
+// Symbol(Symbol.toStringTa
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3)); // to round of decimal number
+// console.log(Math.ceil(4.2)); // to round the number towards upper value
+// console.log(Math.floor(4.8)); // to round of the number towards lower value
+// console.log(Math.min(2,5,7,8)); // to find min value in a arayy
+// console.log(Math.max(2,5,7,8)); // to find msx value in a array
+
+
+console.log(Math.random()); // the value of Math.random always lies between 0 and 1
+console.log(Math.random()*10+1); // not good way to write
+console.log((Math.random()*10)+1); // good way to write
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min + 1)) + min);
+
+
+
+
+
+
